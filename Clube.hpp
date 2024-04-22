12fe861cba906978dd23422b3d0542c75be7ee63
MarceloECOliveira <marceloecoliveira@gmail.com>
2024-04-22 10:37:01 -0300

    a

--- Clube.hpp
+++ Clube.hpp
@@ -14,10 +14,20 @@ class Clube : protected Time {
     protected:
 
     string cidade;
-    vector<Time> clubes;
+    vector<Clube> clubes;
 
     public:
 
     Clube(string nome, int vitorias, int empates, int derrotas, int golsMarcados, int golsSofridos, string cidade) ;
-    
-}
+
+    string getCidade();
+
+    void setCidade(string cidade);
+
+    vector<Clube> getClubes();
+
+    void 
+
+};
+
+#endif /* CLUBE_HPP_ */
